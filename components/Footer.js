deffa04aac638d92e38030c3a17474fd4f4a8159
Orhero Onome <orheroonome@gmail.com>
2022-09-20 17:39:51 +0100

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,84 @@
 import React from 'react'
 
+const arr = [
+  { section0: 'Preferences', products: "Advertise", resources: "List a cryptocurrrency", Info: "About us", social: 'Twitter', logo: '' },
+  { section1: 'Products', products: "Coinranking API ", resources: "Edit a cryptocurrency", Info: "Methodology", social: 'Telegram Group', logo: '' },
+  { section2: 'Resources', products: "Coinhodler", resources: "API documentation", Info: "Changelog", social: 'Telegram Bot', logo: '' },
+  { section3: 'Info', products: "Android app", resources: "API pricing", Info: "Brandiing", social: 'Telegram News', logo: '' },
+  { section: 'Social', products: "Windows app", resources: "Support & FAQ", Info: "Feedback", social: 'Instagram', logo: '' },
+  { resources: "Community Forum", Info: "Terms & conditions", social: 'Facebook', logo: '' },
+  { Info: "Contact", social: 'Linkedln', logo: '' },
+
+]
+
+const foote = [
+  { product: ['Advertise', 'Coinranking API', 'Coinhodler', 'Android app', 'Windows app'] },
+
+]
+const ar = ['preferences', 'products']
+
 const Footer = () => {
   return (
-    <div className='container mx-auto p-6'>
-      my footer
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus ipsum illum quia fugiat autem possimus eius repellat facilis porro?
-    </div>
+    <>
+      <hr />
+      <p className='container mx-auto px-6 py-3 text-sm'>All coins </p>
+      <div className='bg-sky-50 text-sm'>
+        <div className='container mx-auto p-6'>
+          <h1 className='font-bold pb-6'>Partners & clients</h1>
+        </div>
+        <hr />
+        <div className='container mx-auto p-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
+        <ul>
+          <li>Preferences</li>
+          <li> USD American</li>
+          <li> EN English</li>
+        </ul>
+        
+        <ul>
+          <li>Products</li>
+          <li>Advertise</li>
+          <li>Coinranking API</li>
+          <li>Coinholder</li>
+          <li>Android app</li>
+          <li>Windows app</li>
+        </ul>
+
+        <ul>
+        <li> Resources</li>
+          <li> List a cryptocurrency</li>
+          <li>Edit a cryptocurrency </li>
+          <li> API documentation</li>
+          <li> API pricing</li>
+          <li> Support & FAQ</li>
+          <li>Community Forum </li>
+        </ul>
+
+        <ul>
+        <li> Info</li>
+        <li>About us </li>
+        <li>Methodology</li>
+        <li> Changelog</li>
+        <li>Branding </li>
+        <li>Feedback</li>
+        <li>Terms & conditions </li>
+        <li>Contact</li>
+        </ul>
+
+        <ul>
+        <li> Social</li>
+          <li> Twitter </li>
+          <li> Telegram Group</li>
+          <li> Instagram</li>
+          <li>  Facebook</li>
+          <li> LinkedIn </li>
+          <li>Reddit</li>
+          <li>Github</li>
+          <li>Blog</li>
+        </ul>
+        </div>
+      </div>
+
+    </>
   )
 }
 
--- components/MarketStats.js
+++ components/MarketStats.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const MarketStats = ({ stats }) => {
     return (
-        <div>
-            <div className='px-4 container text-sm md:text-base mx-auto grid gap-x-20 gap-y-10 xl:gap-x-24 lg:grid-cols-2 mt-20'>
+        <div className='px-4 container text-sm md:text-base mx-auto'>
+            <div className=' grid gap-x-20 gap-y-10 xl:gap-x-24 lg:grid-cols-2 mt-20'>
                 <div>
                     <h1 className='text-xl md:text-2xl font-bold'>Cryptocurrency Market Statistics</h1>
                     <p>An overview of the complete cryptocurrency market, including the number of cryptocurrencies, the total market cap, and trading volume.</p>
@@ -19,7 +19,7 @@ const MarketStats = ({ stats }) => {
                         <p className='p-4'> All crypto markets<span className='float'>{stats.totalMarkets}</span></p>
                     </div>
                 </div>
-                <div className='bg-sky-100 rounded-lg p-12 text-center'>
+                <div className='bg-sky-50 rounded-lg p-12 text-center'>
                     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima magnam illo quam dolorum deserunt id ratione asperiores facilis voluptatum recusandae molestias nemo distinctio natus nulla repudiandae, eligendi ullam mollitia vero. </h1>
                     <h1 className='text-xl md:text-2xl font-bold'>View crypto prices in Telegram</h1>
                     <p className='py-4'> Instant price updates. 10,000+ cryptocurrencies. Share with friends. </p>
@@ -27,9 +27,21 @@ const MarketStats = ({ stats }) => {
                         View telegram Bot
                     </button>
                 </div>
-            </div>
-            <div className='h-56'>
 
+            </div>
+            <div className='text-base mt-10 mb-5 md:mb-10 bg-sky-50 rounded-lg py-6 text-center grid lg:grid-cols-3'>
+                <div className='lg:text-2xl'>
+                    Coinranking API
+                </div>
+                
+                    <div className='font-bold py-5 lg:py-0'>
+                        Show cryptocurrency data on your website or app
+                    </div>
+                    <div>
+                        <button className='bg-lime-700 hover:bg-blue-300 text-white font-bold py-2 px-6 rounded-full mr-5 lg:mr-0'> View API docs > </button>
+                        <button className='bg-blue-900 hover:bg-blue-300 text-white font-bold py-2 px-6 rounded-full'> Generate API key  </button>
+                    </div>
+               
             </div>
         </div>
     )
